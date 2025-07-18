5eb3794cc357f3c9a32c9291fff3657df02792b3
Ahmad Nur Rozzaq <ahmadnurrozzaq@gmail.com>
2025-07-18 09:35:49 +0700

    menambahkan inisiasi variabel pin signal

--- Miniatur_Bego.ino
+++ Miniatur_Bego.ino
@@ -1,3 +1,22 @@
+const int servo_1 = 11;
+const int servo_2 = 10;
+const int servo_3 = 9;
+const int servo_4 = 6;
+
+const int pb_1_a = 13;
+const int pb_1_b = 12;
+const int pb_2_a = 8;
+const int pb_2_b = 7;
+const int pb_3_a = 5;
+const int pb_3_b = 4;
+const int pb_4_a = 3;
+const int pb_1_b = 2;
+
+const int potensio_1 = A0;
+const int potensio_2 = A1;
+const int potensio_3 = A2;
+const int potensio_4 = A3;
+
 void setup() {
   // put your setup code here, to run once:
 
